669f1e092ae1b2e45a4aee39b1ca0f78e066efb3
Кирилл Сафронов < >
2015-11-05 12:49:03 +0200

    Added parameters constructors in class hieracly

--- InheritanceDemo/main.cpp
+++ InheritanceDemo/main.cpp
@@ -10,6 +10,11 @@ public:
 	char* str;
 	virtual void foo() = 0;
 
+    A(int a, double b)
+    {
+        this->a = a;
+        this->b = b;
+    }
 	void alert()
 	{
 		std::cout << "Attention!!!" << std::endl;
@@ -21,6 +26,12 @@ class B : public A
 {
 public:
 	char c;
+	B(int a, double b, char c)
+        : A(a,b)
+	{
+	    this->c = c;
+
+	}
 	void foo()
 	{
 		cout << "Hello from B\n";
@@ -36,7 +47,10 @@ class C : public A
 public:
 	static const int sz = 3;
 	int m_i[sz];
-	
+
+	C(int a, double b)
+	 : A(a, b)
+	 {}
 	virtual void foo()
 	{
 		for (int i = 0; i < sz; ++i)
@@ -51,12 +65,12 @@ public:
 
 void test_classes1()
 {
-	
 
-	B b1;
+
+	B b1(3,4.5, 'A');
 	b1.alert();
 
-	C c1;
+	C c1(5, -1.4);
 	c1.a = 30;
 	c1.b = 50;
 	c1.foo();
@@ -65,8 +79,8 @@ void test_classes1()
 void test_classes_pointers()
 {
 	A* pA;
-	
-	pA = new B();
+
+	pA = new B(4, 3.2, 'B');
 	pA->foo();
 }
 
